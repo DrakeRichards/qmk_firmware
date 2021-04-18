5c59699226d4e85306ad5db5bc5169f96c795acf
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2021-04-17 22:40:42 -0500

    Lots of fancy toys added

---
+++ keyboards/dztech/dz65rgb/keymaps/drakerichards/config.h
@@ -0,0 +1,26 @@
+//Space Cadet for [] on shift
+#ifdef LSPO_KEYS
+    #undef LSPO_KEYS
+#endif
+
+#define LSPO_KEYS KC_LSFT, KC_TRNS, KC_LBRC
+
+#ifdef RSPC_KEYS
+    #undef RSPC_KEYS
+#endif
+
+#define RSPC_KEYS KC_RSFT, KC_TRNS, KC_RBRC
+
+//Combos
+#ifdef COMBO_COUNT
+    #undef COMBO_COUNT
+#endif
+
+#define COMBO_COUNT 2
+
+//Tapping Term for combos
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+#endif
+
+#define TAPPING_TERM 200
--- keyboards/dztech/dz65rgb/keymaps/drakerichards/keymap.c
+++ keyboards/dztech/dz65rgb/keymaps/drakerichards/keymap.c
@@ -1,17 +1,32 @@
 #include QMK_KEYBOARD_H
 
+//Combos
+enum combos {
+    TD_BSPC,
+    HN_DEL
+};
+
+const uint16_t PROGMEM td_combo[] = {KC_T, KC_D, COMBO_END};
+const uint16_t PROGMEM hn_combo[] = {KC_H, KC_N, COMBO_END};
+
+combo_t key_combos[COMBO_COUNT] = {
+  [TD_BSPC] = COMBO(td_combo, KC_BSPC),
+  [HN_DEL] = COMBO(hn_combo, KC_DEL)
+};
+
+//Layers
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     [0] = LAYOUT_65_ansi(
         KC_GESC,        KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  KC_BSPC, KC_HOME,
         KC_TAB,         KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,    KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_LBRC, KC_RBRC, KC_BSLS, KC_PGUP,
-        LT(1, KC_CAPS), KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,  KC_PGDN,
-        KC_LSFT,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_END,
-        KC_LCTL,        KC_LGUI, KC_LALT,                            KC_SPC,                    KC_RALT, TG(1),   KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+        MO(1),          KC_A,    KC_R,    KC_S,    KC_T,    KC_D,    KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT,          KC_ENT,  KC_PGDN,
+        KC_LSPO,                 KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSPC, KC_UP,   KC_END,
+        KC_LCTL,        KC_LGUI, KC_LALT,     MT(MOD_LSFT, KC_SPC),      LGUI(LCTL(KC_LEFT)), LCTL(LSFT(KC_M)), LGUI(LCTL(KC_RIGHT)), KC_LEFT, KC_DOWN, KC_RIGHT
     ),
     [1] = LAYOUT_65_ansi(
         KC_GESC,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_HOME,
         _______,        RGB_TOG, RGB_MOD, RGB_HUI, RGB_HUD, RGB_SAI, RGB_SAD, RGB_VAI, RGB_VAD, _______, KC_PSCR, KC_SLCK, KC_PAUS, RESET,   KC_PGUP,
-        _______,        RGB_SPI, RGB_SPD, _______, _______, _______, _______, _______, _______, _______, _______, _______,          EEP_RST, KC_PGDN,
+        _______,        RGB_SPI, RGB_SPD, _______, _______, _______, _______, KC_LEFT, KC_UP,   KC_DOWN, KC_RIGHT, _______,         EEP_RST, KC_PGDN,
         KC_LSFT,                 _______, _______, _______, _______, _______, NK_TOGG, _______, _______, _______, _______, _______, KC_VOLU, KC_MUTE,
         _______,        _______, _______,                            _______,                   _______, _______, _______, KC_MPRV, KC_VOLD, KC_MNXT
     )
