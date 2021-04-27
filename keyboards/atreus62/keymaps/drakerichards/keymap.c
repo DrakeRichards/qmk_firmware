007713afd3b663e95fb0403c47834eb07e900927
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2021-04-27 17:10:17 -0500

    Added mouse keys

--- keyboards/atreus62/keymaps/drakerichards/keymap.c
+++ keyboards/atreus62/keymaps/drakerichards/keymap.c
@@ -24,7 +24,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     KC_F12,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_LCBR,                     KC_RCBR, KC_7,    KC_8,    KC_9,    KC_SLASH, KC_TRNS ,
     KC_TRNS,       KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_LPRN,                     KC_RPRN, KC_4,    KC_5,    KC_6,    KC_MINUS, TO(_RESET) ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_LBRC,                     KC_RBRC, KC_1,    KC_2,    KC_3,    KC_PLUS,  KC_TRNS,
+    KC_TRNS,       KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS, KC_LBRC,                     KC_RBRC, KC_1,    KC_2,    KC_3,    KC_PLUS,  KC_TRNS,
     KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_0,    KC_DOT,  KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
