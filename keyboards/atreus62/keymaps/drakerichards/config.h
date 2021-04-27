7735498e264bd2e37ed8a757c35b179403c1e15d
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2021-04-27 12:42:57 -0500

    Fixed Ctrl mistypes

--- keyboards/atreus62/keymaps/drakerichards/config.h
+++ keyboards/atreus62/keymaps/drakerichards/config.h
@@ -2,3 +2,16 @@
     #undef TAPPING_TOGGLE
 #endif
 #define TAPPING_TOGGLE 3
+
+#ifndef IGNORE_MOD_TAP_INTERRUPT
+    #define IGNORE_MOD_TAP_INTERRUPT
+#endif
+
+#ifndef PERMISSIVE_HOLD
+    #define PERMISSIVE_HOLD
+#endif
+
+#ifdef TAPPING_TERM
+    #undef TAPPING_TERM
+#endif
+#define TAPPING_TERM 150
