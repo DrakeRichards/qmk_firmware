b317903dab1f232f770f0dd44706a05ef2119af5
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2021-05-04 12:24:54 -0500

    Changed ctrl

--- keyboards/atreus62/keymaps/drakerichards/config.h
+++ keyboards/atreus62/keymaps/drakerichards/config.h
@@ -1,7 +1,7 @@
 #ifdef TAPPING_TOGGLE
     #undef TAPPING_TOGGLE
 #endif
-#define TAPPING_TOGGLE 3
+#define TAPPING_TOGGLE 2
 
 #ifndef IGNORE_MOD_TAP_INTERRUPT
     #define IGNORE_MOD_TAP_INTERRUPT
--- keyboards/atreus62/keymaps/drakerichards/keymap.c
+++ keyboards/atreus62/keymaps/drakerichards/keymap.c
@@ -9,27 +9,36 @@
 // entirely and just use numbers.
 #define _DEFAULT 0
 #define _NAV 1
-#define _RESET 2
+#define _RIGHT 2
+#define _RESET 3
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [_DEFAULT] = LAYOUT( /* colemak */
-    KC_GESC,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_EQL ,
-    KC_TAB,   KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_MINS,
-    TT(_NAV),   KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT ,
-    LSFT(KC_TAB), KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS ,
-    KC_ENT,   KC_LGUI, KC_LALT, LGUI(LSFT(KC_S)), KC_LALT,LCTL_T(KC_BSPC), KC_LSHIFT,   KC_SPC,  KC_DEL,  LCTL(LGUI(KC_LEFT)),LCTL(LGUI(KC_RIGHT)),KC_LBRC,KC_RBRC,KC_ENT
+    KC_GRAVE,    KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                        KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS ,
+    KC_TAB,     KC_Q,    KC_W,    KC_F,    KC_P,    KC_G,                        KC_J,    KC_L,    KC_U,    KC_Y,    KC_SCLN, KC_PLUS,
+    KC_ESC,     KC_A,    KC_R,    KC_S,    KC_T,    KC_D,                        KC_H,    KC_N,    KC_E,    KC_I,    KC_O,    KC_QUOT ,
+    KC_LSFT,    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,                        KC_K,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_BSLS ,
+    KC_ENT,     KC_LGUI, LGUI(LSFT(KC_S)), KC_LALT, KC_LCTL, KC_LSFT, LT(_NAV, KC_BSPC), KC_DEL, KC_SPC, KC_NO,KC_LEFT,KC_UP,KC_DOWN,KC_RIGHT
   ),
 
   [_NAV] = LAYOUT(
     KC_F12,        KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,                       KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11  ,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_UP,   KC_TRNS, KC_LCBR,                     KC_RCBR, KC_7,    KC_8,    KC_9,    KC_SLASH, KC_TRNS ,
-    KC_TRNS,       KC_TRNS, KC_LEFT, KC_DOWN, KC_RIGHT,KC_LPRN,                     KC_RPRN, KC_4,    KC_5,    KC_6,    KC_MINUS, TO(_RESET) ,
-    KC_TRNS,       KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS, KC_LBRC,                     KC_RBRC, KC_1,    KC_2,    KC_3,    KC_PLUS,  KC_TRNS,
-    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_0,    KC_DOT,  KC_TRNS, KC_TRNS, KC_TRNS
+    KC_TRNS,       KC_HOME, KC_PGUP, KC_PGDN, KC_END, KC_LCBR,                     KC_RCBR, KC_7,    KC_8,    KC_9,    KC_PLUS, KC_TRNS ,
+    KC_TRNS,       KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, KC_LPRN,                     KC_RPRN, KC_4,    KC_5,    KC_6,    KC_MINS, KC_TRNS ,
+    KC_TRNS,       KC_BTN1, KC_BTN2, KC_TRNS, KC_TRNS, KC_LBRC,                     KC_RBRC, KC_1,    KC_2,    KC_3,    KC_TRNS,  KC_TRNS,
+    KC_TRNS,       KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  MO(_RESET),  KC_0, KC_DOT,    LGUI(LCTL(KC_LEFT)),  KC_VOLU, KC_VOLD, LGUI(LCTL(KC_RIGHT))
+  ),
+
+    [_RIGHT] = LAYOUT(
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_LEFT, KC_UP, KC_DOWN, KC_RIGHT, KC_TRNS ,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,                     KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS ,
+    KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_TRNS,  KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS
   ),
 
   [_RESET] = LAYOUT(
-    TO(_DEFAULT),  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
+    KC_NO,  KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
     KC_NO  ,       KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  ,                     KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO  , KC_NO   ,
