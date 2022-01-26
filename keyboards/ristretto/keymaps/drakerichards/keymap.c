0fceecb580a68b6c4dbd7d583bf026f481c41f84
Thomas Young <35073576+DrakeRichards@users.noreply.github.com>
2022-01-25 21:58:48 -0600

    Initial commit

---
+++ keyboards/ristretto/keymaps/drakerichards/keymap.c
@@ -0,0 +1,91 @@
+/* Copyright 2021 Brandon Lewis
+  *
+  * This program is free software: you can redistribute it and/or modify
+  * it under the terms of the GNU General Public License as published by
+  * the Free Software Foundation, either version 2 of the License, or
+  * (at your option) any later version.
+  *
+  * This program is distributed in the hope that it will be useful,
+  * but WITHOUT ANY WARRANTY; without even the implied warranty of
+  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  * GNU General Public License for more details.
+  *
+  * You should have received a copy of the GNU General Public License
+  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+  */
+
+#include QMK_KEYBOARD_H
+#include "oled.c"
+
+enum layers {
+	_BASE,
+	_RAISE,
+	_LOWER,
+	_FUNC,
+    _SYMB,
+	_ADJUST
+};
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+	[_BASE] = LAYOUT(
+		KC_TAB  			, KC_Q    	, KC_W    				, KC_F    			, KC_P    	, KC_G                  , KC_J      , KC_L    			, KC_U      , KC_Y      , KC_SCLN   , KC_MINUS ,
+		LT(_RAISE, KC_ESC)  , KC_A    	, KC_R    				, KC_S    			, KC_T    	, KC_D                  , KC_H      , KC_N    			, KC_E      , KC_I      , KC_O      , KC_QUOT ,
+		KC_LALT 			, KC_Z    	, KC_X    				, KC_C    			, KC_V    	, KC_B      			, KC_K      , KC_M    			, KC_COMM   , KC_DOT    , KC_SLSH   , KC_BSLASH  ,
+	    LCTL_T(KC_ENT) 		, KC_LGUI 	, OSL(_FUNC)	        , LT(_SYMB, KC_BSPC), MO(_LOWER),KC_LSFT, OSM(KC_MEH)   , KC_SPC    , LT(_RAISE, KC_DEL), KC_LEFT   ,KC_UP 	    , KC_DOWN   , KC_RIGHT
+	),
+    [_LOWER] = LAYOUT(
+		KC_GRV              , KC_MINUS  , KC_7                  , KC_8              , KC_9      , KC_LCBR	            , KC_RCBR   , KC_NO             , KC_NO     , KC_NO     , KC_NO     , KC_PLUS  ,
+		KC_TRNS	            , KC_0      , KC_4	                , KC_5              , KC_6      , KC_LPRN               , KC_RPRN   , KC_NO             , KC_NO     , KC_NO     , KC_NO     , KC_EQL   ,
+		KC_TRNS             , KC_DOT    , KC_1                  , KC_2              , KC_3      , KC_LBRC               , KC_RBRC   , KC_NO             , KC_NO     , KC_NO     , KC_NO     , KC_NO   ,
+		KC_TRNS             , KC_TRNS   , KC_TRNS               , KC_TRNS           , KC_TRNS   , KC_TRNS   , KC_NO   , KC_NO     , MO(_ADJUST)       ,KC_HOME    ,KC_PGUP    , KC_PGDN   , KC_END
+	),
+	[_RAISE] = LAYOUT(
+		KC_TRNS	, KC_PGUP	  , KC_HOME    , KC_UP    , KC_END	, KC_NO              , KC_NO    , KC_7    , KC_8    , KC_9    , KC_NO    , KC_NO  ,
+		KC_TRNS	, KC_PGDN , KC_LEFT  , KC_DOWN , KC_RIGHT , KC_NO           , KC_NO   , KC_4 , KC_5 , KC_6   , KC_0 , KC_NO ,
+		KC_TRNS	, KC_NO	  , KC_NO   , KC_NO   , KC_NO   , KC_NO             , KC_NO   , KC_1   , KC_2   , KC_3   , KC_NO   , KC_NO   ,
+		KC_TRNS , KC_TRNS , KC_TRNS ,KC_NO,KC_NO , KC_NO   , KC_NO , KC_NO   , KC_TRNS , KC_NO   , KC_TRNS , KC_TRNS , KC_TRNS
+	),
+	[_FUNC] = LAYOUT(
+		KC_NO	, KC_NO	  , KC_F7    , KC_F8    , KC_F9	, KC_F12              , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO  ,
+		KC_NO	, KC_NO , KC_F4  , KC_F5 , KC_F6 , KC_F11           , KC_NO   , KC_NO , KC_NO , KC_NO   , KC_NO , KC_NO ,
+		KC_NO	, KC_NO	  , KC_F1   , KC_F2   , KC_F3   , KC_F10             , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   , KC_NO   ,
+		KC_NO , KC_NO , KC_TRNS ,KC_NO,KC_NO , KC_NO   , KC_NO , KC_NO   , KC_NO , KC_NO   , KC_NO , KC_NO , KC_NO
+	),
+    [_SYMB] = LAYOUT(
+		KC_TRNS             , KC_TRNS             , KC_AMPR             , KC_ASTR                 , KC_TILDE             , KC_NO	                            , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO  ,
+		KC_TRNS	            , KC_TRNS	            , KC_DLR             , KC_PERC	                , KC_CIRC             , KC_NO                           , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO   ,
+		KC_TRNS             , KC_TRNS             , KC_EXLM             , KC_AT                 , KC_HASH             , KC_NO                           , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO   ,
+		KC_TRNS             , KC_TRNS           , KC_NO                 , KC_TRNS             , KC_NO         , KC_NO       , KC_NO               , KC_NO         , KC_NO         ,KC_NO          , KC_NO         , KC_NO         , KC_NO
+	),
+	[_ADJUST] = LAYOUT(
+		XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+		XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , KC_CAPS           , KC_NLCK , KC_SLCK , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+		XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX           , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX , XXXXXXX ,
+		KC_TRNS , KC_TRNS , KC_TRNS , XXXXXXX , XXXXXXX , XXXXXXX , KC_MUTE , XXXXXXX , XXXXXXX , XXXXXXX , KC_TRNS , KC_TRNS , KC_TRNS
+	)
+/*  [_BLANK] = LAYOUT(
+		KC_TRNS             , KC_NO             , KC_NO                 , KC_NO             , KC_NO         , KC_NO	                            , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO  ,
+		KC_TRNS	            , KC_NO             , KC_NO	                , KC_NO             , KC_NO          , KC_NO                            , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO   ,
+		KC_TRNS             , KC_NO             , KC_NO                 , KC_NO             , KC_NO         , KC_NO                             , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO         , KC_NO   ,
+		KC_TRNS             , KC_TRNS           , KC_NO                 , KC_NO             , KC_NO         , KC_NO       , KC_NO               , KC_NO         , KC_NO         ,KC_NO          , KC_NO         , KC_NO         , KC_NO
+	),*/
+};
+
+// OLED and Encoder function is located in the ristretto.c File
+
+uint16_t cat_timer = 0
+
+bool oled_task_user(void) {
+    switch (get_highest_layer(layer_state)) {
+		case _BASE:
+			render_danceYah();
+			break;
+		case _RAISE:
+			render_danceRight();
+			break;
+		case _LOWER:
+			render_danceLeft();
+			break;
+	}
+    return false;
+}
