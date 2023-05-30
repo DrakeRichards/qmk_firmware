40d84f97f3b72af32d603d2569ab659fecb0e6be
Thomas Young <thomasjyoung8@gmail.com>
2023-05-30 12:33:17 -0500

    Fixed trackpad behavior

--- keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/rules.mk
+++ keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/rules.mk
@@ -1,8 +1,8 @@
 # Cirque settings
-   MOUSEKEY_ENABLE := yes  # not required, but enabling for mouse button keys
-   POINTING_DEVICE_ENABLE := yes
-   POINTING_DEVICE_DRIVER := cirque_pinnacle_i2c
-   OPT_DEFS += -DCIRQUE_ENABLE
+MOUSEKEY_ENABLE := yes  # not required, but enabling for mouse button keys
+POINTING_DEVICE_ENABLE := yes
+POINTING_DEVICE_DRIVER := cirque_pinnacle_i2c
+OPT_DEFS += -DCIRQUE_ENABLE
 
 RGBLIGHT_ENABLE = yes
 OLED_ENABLE = yes
