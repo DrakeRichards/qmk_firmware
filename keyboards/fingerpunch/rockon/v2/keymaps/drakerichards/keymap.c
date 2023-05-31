86496415a3e9ed991b4c5d27811ba6f06724942e
Thomas Young <thomasjyoung8@gmail.com>
2023-05-31 12:12:49 -0500

    Updates for new QMK release

--- keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/keymap.c
+++ keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/keymap.c
@@ -37,7 +37,7 @@ bool idle_enabled = false;
 //                           KC_LCTL,   KC_LGUI,   LOWER,     KC_SPC,    KC_LALT, --->  KC_MUTE, LCTL(KC_BSPC), <--- KC_RALT,   KC_SPC,    RAISE,     KC_RGUI,   KC_RCTL
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
-[_COLEMAK] = LAYOUT_rockon(
+[_COLEMAK] = LAYOUT_default(
     KC_GRV,    KC_EXCLAIM,KC_AT,     KC_HASH,   KC_DOLLAR, KC_PERC,   KC_F5,                              KC_MPRV,   KC_CIRC,   KC_AMPR,   KC_ASTR,   KC_LPRN,   KC_RPRN,   KC_EQL,
     KC_TAB,    KC_Q,      KC_W,      KC_F,      KC_P,      KC_G,      KC_INS,                             KC_MNXT,   KC_J,      KC_L,      KC_U,      KC_Y,      KC_SCLN,   KC_MINS,
     LT(_RAISE,KC_ESC),KC_A,KC_R,     KC_S,      KC_T,      KC_D,      OSM(MOD_MEH),                       KC_MPLY,   KC_H,      KC_N,      KC_E,      KC_I,      KC_O,      KC_QUOT,
@@ -45,7 +45,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_NO,     KC_LGUI,   LT(_LOWER,KC_BSPC),KC_LSFT,KC_LCTL, KC_MUTE, TG(_ADJUST), RAISE,     KC_SPC,    KC_DEL,     TO(_GAMING),   KC_NO
 ),
 
-[_GAMING] = LAYOUT_rockon(
+[_GAMING] = LAYOUT_default(
     KC_GRV,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_LCBR,                            KC_RCBR,   KC_6,      KC_7,      KC_8,      KC_9,      KC_0,      KC_BSPC,
     KC_TAB,    KC_T,      KC_Q,      KC_W,      KC_E,      KC_R,      KC_LPRN,                            KC_RPRN,   KC_Y,      KC_U,      KC_I,      KC_O,      KC_P,      KC_BSLS,
     KC_ESC,    KC_G,      KC_A,      KC_S,      KC_D,      KC_F,      KC_LBRC,                            KC_RBRC,   KC_H,      KC_J,      KC_K,      KC_L,      KC_SCLN,   KC_QUOT,
@@ -53,7 +53,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_NO,     KC_LALT,   KC_LCTL,   KC_SPC,    KC_LSFT,    _______, _______,       KC_RALT,   KC_SPC,    RAISE,     TO(_RUNE),   KC_NO
 ),
 
-[_RUNE] = LAYOUT_rockon(
+[_RUNE] = LAYOUT_default(
     OSM(MOD_LGUI),KC_F1,KC_F2,  KC_F3,   KC_F4, KC_F5,   KC_F5  ,                            KC_MPRV,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,   KC_F11,
     KC_TAB,    KC_1,      KC_2,      KC_3,      KC_4,      KC_5,      KC_GRV ,                            KC_MNXT,   KC_J,      KC_L,      KC_U,      KC_Y,      KC_SCLN,   KC_MINS,
     LT(_RAISE,KC_ESC),KC_F1,KC_F2,     KC_F3,      KC_F4,      KC_F5,      KC_BSPC,                            KC_MPLY,   KC_H,      KC_N,      KC_E,      KC_I,      KC_O,      KC_QUOT,
@@ -61,7 +61,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                           KC_NO,     KC_BTN1,   LT(_LOWER,KC_SPC), KC_LSFT,   KC_LCTL,      KC_MUTE, TG(_ADJUST),   RAISE,     KC_SPC,    KC_DEL,     TO(_COLEMAK),   KC_NO
 ),
 
-[_LOWER] = LAYOUT_rockon(
+[_LOWER] = LAYOUT_default(
     KC_GRV,        _______,       _______,       _______,       _______,       _______,       _______,                              _______,       _______,       _______,       _______,       KC_MINS,       KC_EQL,        _______,
     _______,       KC_MINS,       KC_7   ,       KC_8   ,       KC_9   ,       KC_LCBR,       _______,                              _______,       KC_RCBR,       _______,       _______,       _______,       _______,       _______,
     KC_COLON,      KC_0   ,       KC_4   ,       KC_5   ,       KC_6   ,       KC_LPRN,       LGUI(LCTL(KC_LEFT)),     LGUI(LCTL(KC_RIGHT)),       KC_RPRN,       _______,       _______,       _______,       _______,       _______,
@@ -69,7 +69,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   _______,       KC_IDLE,       _______,       _______,       _______,    _______,       _______,   _______,       _______,       _______,       _______,       _______
 ),
 
-[_RAISE] = LAYOUT_rockon(
+[_RAISE] = LAYOUT_default(
     _______,       _______,       _______,       KC_PGUP,       _______,       _______,       _______,                              _______,       KC_F6  ,       KC_F7  ,       KC_F8  ,       KC_F9  ,       KC_F10,        KC_F11 ,
     _______,       _______,       KC_HOME,       KC_UP  ,       KC_END ,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
     _______,       _______,       KC_LEFT,       KC_DOWN,       KC_RGHT,       _______,       _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
@@ -77,7 +77,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                   _______,       _______,       _______,       _______,       _______,    _______,       _______,   _______,       _______,       _______,       _______,       _______
 ),
 
-[_ADJUST] =  LAYOUT_rockon(
+[_ADJUST] =  LAYOUT_default(
     RESET,         KC_F1,         KC_F2,         KC_F3,         KC_F4,         KC_F5,         _______,                              _______,       KC_F6,         KC_F7,         KC_F8,         KC_F9,         KC_F10,        _______,
     _______,       RGB_TOG,       RGB_RMOD,      RGB_MOD,       _______,       TG(_GAMING),   _______,                              _______,       _______,       _______,       _______,       KC_F11,        KC_F12,        _______,
     _______,       RGB_SPI,       RGB_HUI,       RGB_SAI,       RGB_VAI,       TG(_COLEMAK),  _______,                              _______,       _______,       _______,       _______,       _______,       _______,       _______,
