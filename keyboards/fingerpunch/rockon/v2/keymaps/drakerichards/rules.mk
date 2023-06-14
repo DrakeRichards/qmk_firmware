d8799443029249a5fdee14a76d1b6de61b5a34a1
Thomas Young <thomasjyoung8@gmail.com>
2023-06-14 18:02:41 -0500

    Re-enabled cirque

--- keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/rules.mk
+++ keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/rules.mk
@@ -1,7 +1,6 @@
 # Cirque settings
-CIRQUE_ENABLE = no
 MOUSEKEY_ENABLE := yes  # not required, but enabling for mouse button keys
-POINTING_DEVICE_ENABLE := no
+POINTING_DEVICE_ENABLE := yes
 POINTING_DEVICE_DRIVER := cirque_pinnacle_i2c
 OPT_DEFS += -DCIRQUE_ENABLE
 
@@ -11,5 +10,3 @@ ENCODER_ENABLE = yes
 
 # Stemcell settings
 CONVERT_TO = stemcell
-EEPROM_DRIVER = wear_leveling
-WEAR_LEVELING_DRIVER = legacy
