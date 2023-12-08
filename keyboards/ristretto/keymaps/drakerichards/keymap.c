7f2c14790d4f783d5b30692e0c396e8a112b019a
Thomas <35073576+DrakeRichards@users.noreply.github.com>
2023-12-07 18:59:02 -0600

    Fixed vim arrows

--- keyboards/ristretto/keymaps/drakerichards/keymap.c
+++ keyboards/ristretto/keymaps/drakerichards/keymap.c
@@ -36,25 +36,25 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 		KC_TAB  			, KC_Q    	, KC_W    				, KC_F    			, KC_P    	, KC_G                              , KC_J      , KC_L    			, KC_U      , KC_Y      , KC_SCLN   , KC_MINUS ,
 		LT(_RAISE, KC_ESC)  , KC_A    	, KC_R    				, KC_S    			, KC_T    	, KC_D                              , KC_H      , KC_N    			, KC_E      , KC_I      , KC_O      , KC_QUOT ,
 		KC_LALT 			, KC_Z    	, KC_X    				, KC_C    			, KC_V    	, KC_B      			            , KC_K      , KC_M    			, KC_COMM   , KC_DOT    , KC_SLSH   , KC_BSLS  ,
-	    LT(_SYMB, KC_ENTER) , KC_LGUI 	, OSL(_FUNC)	        , MO(_LOWER)        , KC_LCTL   , LSFT_T(KC_BSPC)   , OSM(MOD_MEH)  , KC_SPC    , LT(_RAISE, KC_DEL), KC_LEFT   ,KC_UP 	    , KC_DOWN   , KC_RIGHT
+	    LT(_SYMB, KC_ENTER) , KC_LGUI 	, OSL(_FUNC)	        , MO(_LOWER)        , KC_LCTL   , LSFT_T(KC_BSPC)   , OSM(MOD_MEH)  , KC_SPC    , LT(_RAISE, KC_DEL), KC_LEFT   ,KC_DOWN 	    , KC_UP   , KC_RIGHT
 	),
 	[_QWER] = LAYOUT(
 		KC_TAB  			, KC_Q    	, KC_W    				, KC_E    			, KC_R    	, KC_T                              , KC_Y      , KC_U    			, KC_I      , KC_O      , KC_P   , KC_MINUS ,
 		LT(_RAISE, KC_ESC)  , KC_A    	, KC_S    				, KC_D    			, KC_F    	, KC_G                              , KC_H      , KC_J    			, KC_K      , KC_L      , KC_SCLN      , KC_QUOT ,
 		KC_LALT 			, KC_Z    	, KC_X    				, KC_C    			, KC_V    	, KC_B      			            , KC_N      , KC_M    			, KC_COMM   , KC_DOT    , KC_SLSH   , KC_BSLS  ,
-	    LT(_SYMB, KC_ENTER) , KC_LGUI 	, KC_BSPC	        , MO(_LOWER)        , KC_LCTL   , KC_LSFT   , KC_SPC  , KC_SPC    , LT(_RAISE, KC_DEL), KC_LEFT   ,KC_UP 	    , KC_DOWN   , KC_RIGHT
+	    LT(_SYMB, KC_ENTER) , KC_LGUI 	, KC_BSPC	        , MO(_LOWER)        , KC_LCTL   , KC_LSFT   , KC_SPC  , KC_SPC    , LT(_RAISE, KC_DEL), KC_LEFT   ,KC_DOWN 	    , KC_UP   , KC_RIGHT
 	),
     [_LOWER] = LAYOUT(
 		KC_GRV              , KC_MINUS  , KC_7                  , KC_8              , KC_9      , KC_LCBR	            , KC_RCBR   , KC_NO             , KC_NO     , KC_NO     , KC_NO     , KC_PLUS  ,
 		KC_COLN	            , KC_0      , KC_4	                , KC_5              , KC_6      , KC_LPRN               , KC_RPRN   , KC_NO             , KC_NO     , KC_NO     , KC_NO     , KC_EQL   ,
 		KC_TRNS             , KC_DOT    , KC_1                  , KC_2              , KC_3      , KC_LBRC               , KC_RBRC   , KC_NO             , KC_NO     , KC_NO     , KC_NO     , KC_NO   ,
-		KC_TRNS             , KC_TRNS   , KC_TRNS               , KC_TRNS           , KC_TRNS   , KC_MUTE   , KC_IDLE   , KC_MPLY     , MO(_ADJUST)       , KC_MPRV    ,KC_VOLU    , KC_VOLD   , KC_MNXT
+		KC_TRNS             , KC_TRNS   , KC_TRNS               , KC_TRNS           , KC_TRNS   , KC_MUTE   , KC_IDLE   , KC_MPLY     , MO(_ADJUST)       , KC_MPRV    ,KC_VOLD    , KC_VOLU   , KC_MNXT
 	),
 	[_RAISE] = LAYOUT(
 		KC_TRNS	, KC_PGUP	  , KC_HOME    , KC_UP    , KC_END	, KC_NO              , KC_NO    , KC_7    , KC_8    , KC_9    , KC_NO    , KC_NO  ,
 		KC_TRNS	, KC_PGDN , KC_LEFT  , KC_DOWN , KC_RIGHT , KC_NO           , KC_NO   , KC_4 , KC_5 , KC_6   , KC_0 , KC_NO ,
 		KC_TRNS	, KC_NO	  , KC_NO   , KC_NO   , KC_NO   , KC_NO             , KC_NO   , KC_1   , KC_2   , KC_3   , KC_NO   , KC_NO   ,
-		KC_TRNS , KC_TRNS , KC_TRNS ,KC_NO,KC_NO , KC_RSFT   , TG(_QWER) , KC_MPLY   , KC_TRNS , LGUI(LCTL(KC_LEFT))   , DT_UP , DT_DOWN , LGUI(LCTL(KC_RIGHT))
+		KC_TRNS , KC_TRNS , KC_TRNS ,KC_NO,KC_NO , KC_RSFT   , TG(_QWER) , KC_MPLY   , KC_TRNS , LGUI(LCTL(KC_LEFT))   , DT_DOWN , DT_UP , LGUI(LCTL(KC_RIGHT))
 	),
 	[_FUNC] = LAYOUT(
 		KC_NO	, KC_NO	  , KC_F7    , KC_F8    , KC_F9	, KC_F12              , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO    , KC_NO  ,
