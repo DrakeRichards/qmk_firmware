c50c7e5cd838ca5459743e83f89b8cf51e058247
Thomas <35073576+DrakeRichards@users.noreply.github.com>
2024-08-10 10:29:49 -0500

    added toggle for number layer when gaming

--- keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/keymap.c
+++ keyboards/fingerpunch/rockon/v2/keymaps/drakerichards/keymap.c
@@ -49,7 +49,7 @@ KC_ENT,     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LALT,
 ),
 
 [_CLG] = LAYOUT(
-KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      KC_F5,                                          KC_MPRV,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_EQL,
+KC_GRV,     KC_F1,      KC_F2,      KC_F3,      KC_F4,      KC_F5,      TG(_LWR),                                          KC_MPRV,    KC_F6,      KC_F7,      KC_F8,      KC_F9,      KC_F10,     KC_EQL,
 KC_TAB,     KC_Q,       KC_W,       KC_F,       KC_P,       KC_G,       KC_INS,                                         KC_MNXT,    KC_J,       KC_L,       KC_U,       KC_Y,       KC_SCLN,    KC_MINS,
 KC_ESC,     KC_A,       KC_R,       KC_S,       KC_T,       KC_D,       MC_OSMM,                                        KC_MPLY,    KC_H,       KC_N,       KC_E,       KC_I,       KC_O,       KC_QUOT,
 KC_ENT,     KC_Z,       KC_X,       KC_C,       KC_V,       KC_B,       KC_LALT,                                        KC_DEL,     KC_K,       KC_M,       KC_COMM,    KC_DOT,     KC_SLSH,    KC_BSLS,
